937486d1390ac6697afcf00dc148205ebb4b8e5b
yasser nascimento <yassern@protonmail.com>
2017-10-09 20:30:19 -0300

    protect account info with dotenv

--- instagram.js
+++ instagram.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 var Client = require('instagram-private-api').V1;
-var device = new Client.Device('user');
+var device = new Client.Device(process.env.INSTAGRAM_USERNAME);
 var storage = new Client.CookieFileStorage(__dirname + '/.cookies/bot-cookies.json');
 
-Client.Session.create(device, storage, 'user', 'password')
+Client.Session.create(device, storage, process.env.INSTAGRAM_USERNAME, process.env.INSTAGRAM_PASSWORD)
   .then(function(session) {
     Client.Upload.photo(session, './road/to/image.jpg')
       .then(function(upload) {
