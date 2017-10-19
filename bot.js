8273a847b17efaa1e03bb5b33bb2a91d03d5befd
Yasser Nascimento <yassern@protonmail.com>
2017-10-19 16:59:50 -0300

    better logs

--- bot.js
+++ bot.js
@@ -3,14 +3,29 @@ var instagramUplouder = require('./instagram');
 var processing = require('./processing');
 var Post = require('./post');
 
+console.log('Bot is running>');
 redditWatcher.on('post', function(post) {
   var data = post.data;
   var post = new Post(data.id, data.domain, data.title, data.author, data.url);
+  console.log('New post> ' + post.id);
   post.download(function(err, files) {
-    if (err) { return; }
+    if (err) {
+      console.log('Error on downloading ' + post.id + ' message>\n' + err.message);
+      return;
+    }
     processing(files, function(err, processedFiles) {
-      if (err) { return; }
-      instagramUplouder(processedFiles, post.createCaption());
+      if (err) {
+        console.log('Error on procesing ' + post.id + ' message>\n' + err.message);
+        return;
+      }
+      instagramUplouder(processedFiles, post.createCaption(), function(err, upload) {
+        if (err) {
+          console.log('Error on posting ' + post.id + ' message>\n' + err.message);
+          return;
+        }
+        console.log('Post ' + post.id + ' uploaded successfully');
+        return upload;
+      });
     })
   });
 })
--- instagram.js
+++ instagram.js
@@ -2,26 +2,28 @@ require('dotenv').config();
 var Client = require('instagram-private-api').V1;
 var device = new Client.Device(process.env.INSTAGRAM_USERNAME);
 var storage = new Client.CookieFileStorage(__dirname + '/.cookies/bot-cookies.json');
-var login = Client.Session.create(device, storage, process.env.INSTAGRAM_USERNAME, process.env.INSTAGRAM_PASSWORD);
+var username = process.env.INSTAGRAM_USERNAME;
+var password = process.env.INSTAGRAM_PASSWORD;
+var login = Client.Session.create(device, storage, username, password);
 
-function upload(path, caption) {
+function upload(path, caption, callback) {
   login
     .then(function(session) {
       if(path.length > 1) {
         Client.Upload.album(session, path)
           .then(function (payload) {
-            return Client.Media.configureAlbum(session, payload, caption, false);
+            callback(null, Client.Media.configureAlbum(session, payload, caption, false));
           })
-          .catch(function(err) { return; });
+          .catch(function(err) { callback(err, null) });
       } else {
         Client.Upload.photo(session, path[0])
           .then(function(upload) {
-            return Client.Media.configurePhoto(session, upload.params.uploadId, caption);
+            callback(null, Client.Media.configurePhoto(session, upload.params.uploadId, caption));
           })
-          .catch(function(err) { return; });
+          .catch(function(err) { callback(err, null) });
       }
     })
-    .catch(function(err) { return; });
+    .catch(function(err) { callback(err, null) });
 }
 
 module.exports = upload;
